b0b9c2e87b9fd54ab6ee98c89d2258aa3a69a28e
Mike <trikefx@live.co.uk>
2020-08-19 17:16:01 +0100

    + Added escosystem config file for PM2.

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "lucille",
+      script: "./index.js",
+      out_file: "./logs/out.log",
+      error_file: "./logs/err.log",
+      time: true,
+    },
+  ],
+}
