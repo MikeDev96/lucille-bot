5e116e6b6e775221c9753a0eddc2e0756392bfac
JoeC7756 <55831387+JoeC7756@users.noreply.github.com>
2020-10-11 23:08:41 +0100

    Update violin.js

--- commands/fun/violin.js
+++ commands/fun/violin.js
@@ -1,6 +1,6 @@
 const { Command } = require("discord.js-commando")
 
-module.exports = class CrapCommand extends Command {
+module.exports = class ViolinCommand extends Command {
   constructor (client) {
     super(client, {
       name: "violin",
