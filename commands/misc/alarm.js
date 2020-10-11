a3e3aa045cef61a0e95ee124d786d9f6f9bd5223
JoeC7756 <joecollins7736@gmail.com>
2020-10-11 17:04:02 +0100

    Multiple timezones

--- commands/misc/alarm.js
+++ commands/misc/alarm.js
@@ -1,4 +1,5 @@
 const { Command } = require("discord.js-commando")
+const { msToTimestamp } = require('../../helpers')
 
 module.exports = class Alarm extends Command {
   constructor (client) {
@@ -15,44 +16,63 @@ module.exports = class Alarm extends Command {
           type: "string",
           validate: val => {
             
-            let args = val.split(' ')
-
-            if(args.length > 1)
-                return false
-            
-            if(args[0].match(new RegExp('(2[0-3]|[0-1]?[\\d]):[0-5][\\d]')) && args[0].length === 5) 
+            if(val.match(new RegExp('(2[0-3]|[0-1]?[\\d]):[0-5][\\d]')) && val.length === 5) 
                 return true
             
             return "Please provide a time in the format hh:mm (UTC Time)"
-
           }
         },
+        {
+          key: "timezoneDifference",
+          prompt: "How many hours ahead or behind UTC",
+          type: "string",
+          validate: val => {
+
+            val = Number(val)
+
+            if(isNaN(val))
+              return "Please return a number"
+  
+            if(Math.abs(val) > 12)
+              return "Please provide a number between -12 and 12"
+
+            return true
+          }
+        }
       ],
       guildOnly: true,
     })
   }
 
-  async run (msg, args) {
-    msg.react("⏲️")
+  TimeDiffCalc(msg, args) {
 
     let StartTime = new Date().toLocaleTimeString('UTC')
 
-    console.log(args.time.split(' ')[0])
-
     // Time difference in ms
-    let DateOne = new Date(`January 1 2000, ${args.time.split(' '[0])}:00`)
+    let DateOne = new Date(`January 1 2000, ${args.time}:00`)
     let DateTwo = new Date(`January 1 2000, ${StartTime}`)
 
     let TimeDif = DateOne - DateTwo
-
+    
     // Add a day if needed
-    if(DateOne - DateTwo <= 0)
+    if(TimeDif <= 0)
         TimeDif += 86400000
 
-    setTimeout(async () => {
-        await msg.channel.send(`${msg.author}'s Alarm has finished`)
-    }, TimeDif)
+    TimeDif += (args.timezoneDifference * 3600000) 
 
+    let timestamp = msToTimestamp(TimeDif).toString()
+
+    msg.reply(`An alarm has been set for ${isNaN(timestamp) ? timestamp : 0}s time`)
+
+    return TimeDif
+  }
+
+  async run (msg, args) {
+    msg.react("⏲️")
+
+    setTimeout(async () => {
+        await msg.channel.send(`${msg.author} Your ${args.time} Alarm has finished`)
+    }, this.TimeDiffCalc(msg, args))
   }
   
 }
