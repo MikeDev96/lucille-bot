d4603c159a8bc9c33cb7810cf0baa883c959a352
Deff3x <deathy0909@gmail.com>
2020-10-14 21:13:25 +0100

    + Added alias so Lucille can run it via voice

--- commands/fun/pp.js
+++ commands/fun/pp.js
@@ -5,7 +5,7 @@ module.exports = class extends Command {
   constructor(client) {
     super(client, {
       name: "pp",
-      aliases: [],
+      aliases: ["penis"],
       group: "fun",
       memberName: "pp",
       description: "Penis Length",
