b97f5dcfcdcda0702cf4183f5200b1e7fab1f771
Deff3x <deathy0909@gmail.com>
2020-10-14 18:33:18 +0100

    + Added MasterDatabase class that uses sqlite3 + PP command now supports permanent pps + Added extra line to VoiceTracker to insert displaynames + Update index.js to include this.db as the reference to MasterDatabase

---
+++ classes/MasterDatabase.js
@@ -0,0 +1,98 @@
+const sqlite = require("better-sqlite3")
+
+module.exports = class {
+  constructor() {
+    this.db = new sqlite("main.db", { readonly: false })
+    this.initTables()
+  }
+
+  // probably needs some proper ERD setting up..
+  initTables() {
+    // PenisSize command
+    this.db.exec(`
+      CREATE TABLE IF NOT EXISTS PenisSize (
+        Id          INTEGER PRIMARY KEY AUTOINCREMENT,
+        UserId	 		TEXT,
+        ServerId    TEXT, 
+        Size		    INTEGER
+      ) 
+    `)
+
+      // DISPLAY NAME
+      .exec(`
+       CREATE TABLE IF NOT EXISTS UserInfo (
+          UserId	 		PRIMARY KEY,
+          DisplayName TEXT
+        ) 
+      `)
+
+    console.log("main.db ready")
+  }
+
+  /**
+   * Query is expected to run without returning any data
+   * @param {string} query - Parameter based SQL query 
+   * @param  {...any} params - Params provided to the query
+   */
+  run(query, ...params) {
+    const stmt = this.db.prepare(query)
+    const result = stmt.run(...params)
+    return result
+  }
+
+  /**
+   * Query is expected to an array of results
+   * @param {string} query - Parameter based SQL query 
+   * @param  {...any} params - Params provided to the query
+   */
+  runQuery(query, ...params) {
+    const stmt = this.db.prepare(query)
+    const result = stmt.all(...params)
+    return result
+  }
+
+  /**
+   * Query is expected to return 1 result
+   * @param {string} query - Parameter based SQL query 
+   * @param  {...any} params - Params provided to the query
+   */
+  runScalarQuery(query, ...params) {
+    const stmt = this.db.prepare(query)
+    const result = stmt.get(...params)
+    return result
+  }
+
+  /**
+   * Used for getting the Authors penis size, not server wide
+   * @param {string} user - Discord author id 
+   */
+  getPenisSize(user, server, $default) {
+    const rslts = this.runScalarQuery("SELECT Size FROM PenisSize WHERE UserId = ? AND ServerId = ?", user, server)
+
+    // if rslts is undefined we didn't find a user in the db so insert one.
+    if (!rslts) {
+      this.run("INSERT INTO PenisSize ([UserId], [ServerId], [Size]) VALUES (?, ?, ?)", user, server, $default)
+      return $default
+    }
+
+    return rslts.Size
+  }
+
+  getAllPenisSize(serverId) {
+    const rslts = this.runQuery("SELECT * FROM PenisSize ps INNER JOIN UserInfo ui ON ps.UserId = ui.UserId WHERE ps.ServerId = ?", serverId)
+    return !rslts ? [] : rslts;
+  }
+
+  updateUser(userId, displayName) {
+    const rslts = this.runScalarQuery("SELECT userId FROM UserInfo WHERE UserId = ?", userId)
+    if (rslts) {
+      this.run("UPDATE UserInfo SET [DisplayName] = ? WHERE [UserId] = ?", displayName, userId)
+    } else {
+      this.run("INSERT INTO UserInfo ([UserId], [DisplayName]) VALUES (?, ?)", userId, displayName)
+    }
+  }
+
+  getDisplayName(userId) {
+    return this.runScalarQuery("SELECT DisplayName FROM UserInfo WHERE UserId = ?", userId)
+  }
+}
