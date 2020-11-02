44f471424a499151f9d55041725a8edfdb919dfe
JoeC7756 <joecollins7736@gmail.com>
2020-11-02 20:03:24 +0000

    ++Add spotify exporter

--- classes/BangaTracker.js
+++ classes/BangaTracker.js
@@ -2,80 +2,84 @@ const low = require("lowdb")
 const FileSync = require("lowdb/adapters/FileSync")
 
 module.exports = class {
-  constructor (client) {
+  constructor(client) {
     this.client = client
     this.monitor = {}
     this.initDB()
   }
 
-  initDB () {
+  initDB() {
     const adapter = new FileSync("banga.json")
     this.db = low(adapter)
 
-    this.db.defaults({ bangers: [], poopers: []})
+    this.db.defaults({ bangers: [], poopers: [] })
       .write()
   }
 
-  writeBanga(banger, user) {
+  writeBanga(spotifyUri, banger, user) {
     this.db.get("bangers")
-        .push({song: banger, users: [user]})
-        .write();
+      .push({
+        song: banger,
+        users: [user],
+        spotifyUri: spotifyUri ? spotifyUri : ""
+      })
+      .write();
   }
 
   checkForBanga(banger) {
     return this.db.get("bangers")
-        .filter({song: banger})
-        .take(1)
-        .value();
+      .filter({ song: banger })
+      .take(1)
+      .value();
   }
 
   updateUsers(banger, user) {
     let data = this.db.get("bangers")
-                .filter({song: banger})
-                .take(1)
-                .value();
+      .filter({ song: banger })
+      .take(1)
+      .value();
 
     data[0].users.push(user);
 
     this.db.get("bangers")
-        .find({song: banger})
-        .assign(data[0])
-        .write();
+      .find({ song: banger })
+      .assign(data[0])
+      .write();
   }
 
   removeBanga(banger, user) {
     let data = this.db.get("bangers")
-                .find(e => {
-                  return e.song.toLowerCase().includes(banger.toLowerCase()) && (e.users.indexOf(user) > -1)
-                })
-                .value();
+      .find(e => {
+        return e.song.toLowerCase().includes(banger.toLowerCase()) && (e.users.indexOf(user) > -1)
+      })
+      .value();
 
     const index = data.users.indexOf(user)
 
-    if(index > -1) data.users.splice(index, 1)
-    
+    if (index > -1) data.users.splice(index, 1)
+
     this.db.get("bangers")
-      .find({song: banger})
+      .find({ song: banger })
       .assign(data)
       .write();
   }
 
   findBanga(banger, user) {
     let data = this.db.get("bangers")
-                .find(e => {
-                  return e.song.toLowerCase().includes(banger.toLowerCase()) && (e.users.indexOf(user) > -1)
-                })
-                .value();
-    
+      .find(e => {
+        return e.song.toLowerCase().includes(banger.toLowerCase()) && (e.users.indexOf(user) > -1)
+      })
+      .value();
 
-    if(!data) data = {song: null}
-    
+
+    if (!data) data = { song: null }
+
     return data.song;
   }
 
   listBangas(user) {
     return this.db.get("bangers")
-      .filter({users: [user]})
+      .filter({ users: [user] })
       .value();
   }
 }
