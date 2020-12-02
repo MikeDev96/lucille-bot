728dd00bad0116fbaef87e8b0fa9f3007da01329
JoeC7756 <joecollins7736@gmail.com>
2020-12-02 20:10:04 +0000

    ~daily leaderboard fix and testing

--- commands/fun/aocleaderboard.js
+++ commands/fun/aocleaderboard.js
@@ -14,40 +14,6 @@ module.exports = class extends Command {
         })
     }
 
-    static async getLeaderboard() {
-
-        //Query AOC - Cookie expires in 2030, so should be good for the time being
-        let res = await axios.get("https://adventofcode.com/2020/leaderboard/private/view/1064962.json", {
-            "headers": {
-                "cookie": config.aoc.cookie
-            }
-        });
-
-        let LBInfo = res.data
-        const medals = ["🥇", "🥈", "🥉"]
-
-        let leaderboard = {
-            embed: {
-                color: 0x0099ff,
-                title: ":christmas_tree: Advent of Code leaderboard 2020 :christmas_tree:",
-                description: `If you wish to join, [join here](https://adventofcode.com/2020/leaderboard/private) with this code **${config.aoc.leaderboard}**`,
-                fields: [
-                    // Get data filter by local score and then map through
-                    ...Object.values(LBInfo.members).sort((a, b) => b.local_score - a.local_score).map((member, index) =>
-                        ({
-                            name: `${medals[index] || ""} ${member.name} (Score: ${member.local_score})`,
-                            value: (member.stars > 0 && member.stars < 10) ? ":star:".repeat(member.stars) : `${member.stars} :star:`
-                        })),
-                ],
-                footer: {
-                    text: config.discord.footer,
-                    icon_url: config.discord.authorAvatarUrl,
-                }
-            }
-        }
-        return leaderboard
-    }
-
     async run(msg, _args) {
         let leaderboard = await getLeaderboard()
         msg.channel.send(leaderboard)
@@ -61,4 +27,39 @@ module.exports = class extends Command {
 
         firstGuildChannel.send(leaderboard)
     }
+}
+
+// Get leaderboard function being removed from the class seems to let it be posted daily
+async function getLeaderboard() {
+
+    //Query AOC - Cookie expires in 2030, so should be good for the time being
+    let res = await axios.get("https://adventofcode.com/2020/leaderboard/private/view/1064962.json", {
+        "headers": {
+            "cookie": config.aoc.cookie
+        }
+    });
+
+    let LBInfo = res.data
+    const medals = ["🥇", "🥈", "🥉"]
+
+    let leaderboard = {
+        embed: {
+            color: 0x0099ff,
+            title: ":christmas_tree: Advent of Code leaderboard 2020 :christmas_tree:",
+            description: `If you wish to join, [join here](https://adventofcode.com/2020/leaderboard/private) with this code **${config.aoc.leaderboard}**`,
+            fields: [
+                // Get data filter by local score and then map through
+                ...Object.values(LBInfo.members).sort((a, b) => b.local_score - a.local_score).map((member, index) =>
+                    ({
+                        name: `${medals[index] || ""} ${member.name} (Score: ${member.local_score})`,
+                        value: (member.stars > 0 && member.stars < 10) ? ":star:".repeat(member.stars) : `${member.stars} :star:`
+                    })),
+            ],
+            footer: {
+                text: config.discord.footer,
+                icon_url: config.discord.authorAvatarUrl,
+            }
+        }
+    }
+    return leaderboard
 }
