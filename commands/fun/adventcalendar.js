f1526c72922ad7b96d80261e002f7fceee148080
katherinefrancis <jakewych98@gmail.com>
2020-12-04 22:07:22 +0000

    katherine's first coding adventure

--- commands/fun/adventcalendar.js
+++ commands/fun/adventcalendar.js
@@ -6,9 +6,6 @@ const Tenor = require("tenorjs").client({
     "Locale": "en_US",
 });
 
-var date = String(new Date())
-let month = date.substring(4, 7);
-let day = parseInt(date.substring(8, 10));
 var gifpairing = {}
 
 var keywords = ["christmas pudding", "christmas donkey", "christmas elf", "christmas snowman", "christmas present", "christmas dog", "christmas cake", "christmas baby", "christmas jumper", "christmas bauble", "christmas wreath", "christmas stocking", "christmas robin", "santa sleigh", "rudolph", "christmas candle", "christmas tree", "christmas michael buble", "christmas hat", "christmas cookie", "christmas candy cane", "christmas lights", "christmas cracker", "father christmas", "christmas dinner"]
@@ -27,6 +24,9 @@ module.exports = class extends Command {
   }
 
   run(msg, args) {
+    var date = String(new Date())
+    let month = date.substring(4, 7);
+    let day = parseInt(date.substring(8, 10));
     if(month !== "Dec"){
         msg.reply("bugger off it's not christmas silly")
 
