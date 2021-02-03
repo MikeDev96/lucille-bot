24e5f30c967560c091622dd87bac3fa624754367
MikeDev96 <trikefx@live.co.uk>
2021-02-03 19:02:29 +0000

    + Added more http headers to Amazon ripper in attempt to bypass captcha.

--- classes/AmazonRipper.js
+++ classes/AmazonRipper.js
@@ -61,7 +61,20 @@ const AmazonRipper = class {
 
   static async getInfo (url) {
     try {
-      const res = await fetch(url, { headers: { "User-Agent": "Lucille/1.0.0" } }) // PostmanRuntime/7.26.8 | Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36
+      const res = await fetch(url, {
+        headers: {
+          Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Accept-Language": "en",
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "none",
+          "Sec-Fetch-User": "?1",
+          "Upgrade-Insecure-Requests": "1",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+        },
+      })
+
       const html = await res.text()
 
       const jsonMatch = /var obj = jQuery\.parseJSON\('(.+?)'\)/.exec(html)
