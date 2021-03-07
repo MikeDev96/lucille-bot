ad024a28f206c857c022840c81eadbd138acb1fe
MikeDev96 <trikefx@live.co.uk>
2021-03-07 22:59:58 +0000

    Revert "Added next imposter release to imposter command"

--- commands/fun/imposter.js
+++ commands/fun/imposter.js
@@ -1,8 +1,5 @@
 const { Command } = require("discord.js-commando")
 const { discord } = require("../../config")
-const countdown = require("countdown")
-const axios = require("axios")
-const moment = require("moment")
 
 module.exports = class extends Command {
   constructor (client) {
@@ -17,166 +14,31 @@ module.exports = class extends Command {
     })
   }
 
-  run (msg) {
-
-    axios.get("http://server.haribodev.uk/imposter/versions/update")
-      .then(res => {
-        var day = Number(res.data.substring(0, 2))
-        var month = Number(res.data.substring(3, 5))
-        var year = Number(res.data.substring(6, 10))
-
-        var time = res.data.substring(11)
-
-        var countDown = countdown(new Date(year, month - 1, day).setHours(18)).toString()
-        countDown = countDown.replace(/, /g, "\n").replace(/ and /g, "\n")
-
-        const embed = {
-          embed: {
-            color: 0x0099ff,
-            title: "Imposter Update",
-            description: "We're just as excited as you are, but how long do we have to wait?",
-            author: {
-              name: msg.member.displayName,
-              icon_url: msg.author.displayAvatarURL(),
-            },
-            fields: [
-              {
-                name: "Next Update",
-                value: res.data === "N/A" ? "Hmm looks like there's no release date as of yet, but stay tuned!" : `${moment(new Date(year, month - 1, day)).format("dddd Do MMMM YYYY")} at ${time}`,
-              },
-              {
-                name: "Countdown",
-                value: countDown,
-              },
-              {
-                name: "Updates",
-                value: `[View updates](https://launcher.haribodev.uk/news)`,
-              },
-              {
-                name: "HariboDev Launcher",
-                value: `[Download the HariboDev Launcher](https://launcher.haribodev.uk)`,
-              },
-            ],
-            footer: {
-              text: discord.footer,
-            },
+  run (msg, args) {
+    const embed = {
+      embed: {
+        color: 0x0099ff,
+        title: "Imposter Release",
+        description: "It's already been released dum dum.",
+        author: {
+          name: msg.member.displayName,
+          icon_url: msg.author.displayAvatarURL(),
+        },
+        fields: [
+          {
+            name: "Updates",
+            value: `[View updates](https://launcher.haribodev.uk/news)`,
           },
-        }
-        msg.reply(embed)
-      })
-      .catch(err => {
-        const embed = {
-          embed: {
-            color: 0x0099ff,
-            title: "Imposter Update",
-            description: "We're just as excited as you are, but how long do we have to wait?",
-            author: {
-              name: msg.member.displayName,
-              icon_url: msg.author.displayAvatarURL(),
-            },
-            fields: [
-              {
-                name: "Next Update",
-                value: "Hmm, something went wrong. Check the dev console!",
-              },
-              {
-                name: "Updates",
-                value: `[View updates](https://launcher.haribodev.uk/news)`,
-              },
-              {
-                name: "HariboDev Launcher",
-                value: `[Download the HariboDev Launcher](https://launcher.haribodev.uk)`,
-              },
-            ],
-            footer: {
-              text: discord.footer,
-            },
+          {
+            name: "HariboDev Launcher",
+            value: `[Download the HariboDev Launcher](https://launcher.haribodev.uk)`,
           },
-        }
-        msg.reply(embed)
-        console.log(err)
-      })
-  }
-
-  static imposterDailyReminder (client, guild) {
-    axios.get("http://server.haribodev.uk/imposter/versions/update")
-      .then(res => {
-        var day = Number(res.data.substring(0, 2))
-        var month = Number(res.data.substring(3, 5))
-        var year = Number(res.data.substring(6, 10))
-
-        var time = res.data.substring(11)
-
-        var countDown = countdown(new Date(year, month - 1, day).setHours(18)).toString()
-        countDown = countDown.replace(/, /g, "\n").replace(/ and /g, "\n")
-
-        const dailyEmbed = {
-          embed: {
-            color: 0x0099ff,
-            title: "Imposter Update",
-            description: "We're just as excited as you are, but how long do we have to wait?",
-            author: {
-              name: "HariboDev",
-              icon_url: client.user.displayAvatarURL(),
-            },
-            fields: [
-              {
-                name: "Next Update",
-                value: res.data === "N/A" ? "Hmm looks like there's no release date as of yet, but stay tuned!" : `${moment(new Date(year, month - 1, day)).format("dddd Do MMMM YYYY")} at ${time}`,
-              },
-              {
-                name: "Countdown",
-                value: countDown,
-              },
-              {
-                name: "Updates",
-                value: `[View updates](https://launcher.haribodev.uk/news)`,
-              },
-              {
-                name: "HariboDev Launcher",
-                value: `[Download the HariboDev Launcher](https://launcher.haribodev.uk)`,
-              },
-            ],
-            footer: {
-              text: discord.footer,
-            },
-          },
-        }
-        const firstGuildChannel = guild.channels.cache.filter(channel => channel.type === "text").first()
-        firstGuildChannel.send(dailyEmbed)
-      })
-      .catch(err => {
-        const dailyEmbed = {
-          embed: {
-            color: 0x0099ff,
-            title: "Imposter Update",
-            description: "We're just as excited as you are, but how long do we have to wait?",
-            author: {
-              name: "HaribuDev",
-              icon_url: client.user.displayAvatarURL(),
-            },
-            fields: [
-              {
-                name: "Next Update",
-                value: "Hmm, something went wrong. Check the dev console!",
-              },
-              {
-                name: "Updates",
-                value: `[View updates](https://launcher.haribodev.uk/news)`,
-              },
-              {
-                name: "HariboDev Launcher",
-                value: `[Download the HariboDev Launcher](https://launcher.haribodev.uk)`,
-              },
-            ],
-            footer: {
-              text: discord.footer,
-            },
-          },
-        }
-        console.log(err)
-        const firstGuildChannel = guild.channels.cache.filter(channel => channel.type === "text").first()
-        firstGuildChannel.send(dailyEmbed)
-      })
+        ],
+        footer: {
+          text: discord.footer,
+        },
+      },
+    }
+    msg.reply(embed)
   }
 }
