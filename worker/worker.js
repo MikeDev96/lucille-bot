aeac7a4cc5a13c483d551b4328f7c057bfdd6e56
Mike <trikefx@live.co.uk>
2022-01-25 14:39:43 +0000

    Revert "+ Added a points system to the YouTube search to try and prioritise lyric/radio edit songs."
    This reverts commit e53c38e30a10fcee449a5dc9756af84f02ed8a7b.

--- worker/worker.js
+++ worker/worker.js
@@ -4,14 +4,7 @@ const AmazonRipper = require("../classes/AmazonRipper")
 
 const searchYouTube = async query => {
   const searchResults = (await scrapeYt.search(query)).filter(res => res.type === "video")
-
-  const patterns = ["radio edit", "radio", "audio only", "audio", "official audio", "official lyrics?", "lyrics?"]
-
-  const sortedResults = searchResults
-    .map((item, idx) => ({ info: item, pts: patterns.reduce((acc, cur) => acc + new RegExp(`\\b${cur}\\b`, "i").test(item.title), 0), order: idx }))
-    .sort((a, b) => b.pts - a.pts || a.order - b.order)
-
-  return sortedResults.map(item => item.info)
+  return searchResults
 }
 
 const getAmazonInfo = async url => await AmazonRipper.getInfo(url)
