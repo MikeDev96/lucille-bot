44210a621a7884f67538dde6db1b26c21a9db300
Mike <trikefx@live.co.uk>
2022-02-01 16:13:51 +0000

    ~ Updated YouTube searching library. ~ Updated ytdl-core library.

--- worker/worker.js
+++ worker/worker.js
@@ -1,10 +1,18 @@
 const workerpool = require("workerpool")
-const scrapeYt = require("scrape-yt")
+const { Client } = require("youtubei")
 const AmazonRipper = require("../classes/AmazonRipper")
 
+const youtube = new Client()
+
 const searchYouTube = async query => {
-  const searchResults = (await scrapeYt.search(query)).filter(res => res.type === "video")
-  return searchResults
+  const searchResults = await youtube.search(query, { type: "video" })
+
+  return searchResults.map(v => ({
+    id: v.id,
+    title: v.title,
+    thumbnail: v.thumbnails[0].url,
+    duration: v.duration,
+  }))
 }
 
 const getAmazonInfo = async url => await AmazonRipper.getInfo(url)
