ec1d25e51735f3c5b26d65418bd311e925609af6
JWK95 <jono347moo12@yahoo.co.uk>
2022-03-02 00:35:04 +0000

    --fixed prod issues

---
+++ commands/misc/lmgtfy.js
@@ -0,0 +1,25 @@
+const { Command } = require("discord.js-commando")
+
+module.exports = class extends Command {
+  constructor (client) {
+    super(client, {
+      name: "letmegooglethatforyou",
+      aliases: ["lmgtfy"],
+      group: "misc",
+      memberName: "lmgtfy",
+      description: "A response to a snooty response to someone asking for help",
+      args: [],
+      guildOnly: true,
+    })
+  }
+
+  async run (msg, args) {
+    if (encodeURI("https://letmegooglethat.com/?q=" + args).length > 1999) {
+      args = "That was too big"
+      msg.reply(`The person was just asking for help, ${msg.author.username} you wet egg. ${encodeURI("https://letmegooglethat.com/?q=" + args)}`)
+    }
+    else {
+      msg.reply(`The person was just asking for help, ${msg.author.username} you wet egg. ${encodeURI("https://letmegooglethat.com/?q=" + args)}`)
+    }
+  }
+}
